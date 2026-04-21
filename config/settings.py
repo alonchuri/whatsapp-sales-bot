553185faed7a8fbb4ce9e8f9f29bb8e21a1b22cf
alonchuri <alonchuri@churishop.co.il>
2026-04-21 18:47:12 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,10 @@ load_dotenv()
 
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "")
-GOOGLE_SERVICE_ACCOUNT_FILE = os.getenv("GOOGLE_SERVICE_ACCOUNT_FILE", "service_account.json")
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
+GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI", "")
 GOOGLE_DRIVE_FOLDER_ID = os.getenv("GOOGLE_DRIVE_FOLDER_ID", "")
 FLIPPINGBOOK_API_KEY = os.getenv("FLIPPINGBOOK_API_KEY", "")
 FLIPPINGBOOK_API_URL = os.getenv("FLIPPINGBOOK_API_URL", "https://api.flippingbook.com/v1")
+TOKEN_FILE = "/tmp/google_token.json"
